d5a007d2e900a31be392244f050af1175cad53ba
Elaine <elainelozanobelmonte@gmail.com>
2024-01-30 20:25:53 +0800

    model creation trial

--- dormitelSystem/dms/models.py
+++ dormitelSystem/dms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Accounts(models.Model):
+    accountID = models.IntegerField()
+    fullname = models.CharField(max_length = 100)
+    username = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 100)
+
--- dormitelSystem/dormitelSystem/settings.py
+++ dormitelSystem/dormitelSystem/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dms'
+    #'dms.apps.dmsConfig'
 ]
 
 MIDDLEWARE = [
