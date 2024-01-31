50ddcdde06f174bee23270437e1c612fbbe9f15c
Elaine <elainelozanobelmonte@gmail.com>
2024-01-31 16:00:18 +0800

    forms

--- dormitelSystem/dms/models.py
+++ dormitelSystem/dms/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Accounts(models.Model):
-    accountID = models.IntegerField()
-    fullname = models.CharField(max_length = 100)
-    username = models.CharField(max_length = 100)
-    password = models.CharField(max_length = 100)
+class Lodger(models.Model):
+    lodgerID = models.CharField(max_length = 50 unique = True)
+    last_name = models.CharField(max_length = 100)
+    first_name = models.CharField(max_length=50)
+    username = models.CharField(max_length = 50)
+    password = models.CharField(max_length = 50)
 
