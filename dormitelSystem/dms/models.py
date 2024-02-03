3f4d788d24ac66525918cd32791dd673dacc39eb
Elaine <elainelozanobelmonte@gmail.com>
2024-02-03 09:08:18 +0800

    form enhancements

--- dormitelSystem/dms/models.py
+++ dormitelSystem/dms/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Lodger(models.Model):
-    lodgerID = models.CharField(max_length = 50, unique = True)
-    last_name = models.CharField(max_length = 100)
-    first_name = models.CharField(max_length=50)
+    lodgerID = models.CharField(max_length = 50, unique = True, null=False)
+    last_name = models.CharField(max_length = 100,null=False)
+    first_name = models.CharField(max_length=50, null=False)
     contact_number = models.IntegerField()
     home_address = models.CharField(max_length = 100)
-    email_address = models.CharField(max_length = 254)
+    email_address = models.CharField(max_length = 254, null=False)
     username = models.CharField(max_length = 50)
     password = models.CharField(max_length = 50)
     schedule_of_last_due = models.DateField() #look for the correct datatype of this field
     schedule_of_next_payment = models.DateField() #look for the correct datatype of this field
 
 class Rooms(models.Model):
-    room_number = models.IntegerField(unique=True)
-    room_description = models.TextField()
+    room_number = models.IntegerField(unique=True, null=False)
+    room_description = models.TextField(null=False)
     room_type = models.CharField(max_length = 20)
-    room_amount = models.FloatField()
-    room_status = models.CharField(max_length=20)
-    no_of_beds_available = models.IntegerField()
+    room_amount = models.FloatField(null=False)
+    room_status = models.CharField(max_length=20, null=False)
+    no_of_beds_available = models.IntegerField(null=False)
 
 class Beds(models.Model):
     bed_no = models.IntegerField()
