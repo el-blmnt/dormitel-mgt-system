da7e667f77f914eeb9683f30d5a4ef4fedeb828c
Elaine <elainelozanobelmonte@gmail.com>
2024-02-18 11:11:41 +0800

    model enhancements

--- dormitelSystem/dms/models.py
+++ dormitelSystem/dms/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Lodger(models.Model): 
-    lodgerID = models.CharField(max_length = 50, unique = True, null=False)
-    last_name = models.CharField(max_length = 100,null=False)
-    first_name = models.CharField(max_length=50, null=False)
+    lodgerID = models.CharField(max_length = 50, default="", unique = True, null=False)
+    last_name = models.CharField(max_length = 100, default="", null=False)
+    first_name = models.CharField(max_length=50, default="", null=False)
     contact_number = models.IntegerField()
-    home_address = models.CharField(max_length = 100)
-    email_address = models.EmailField(null=False)
-    username = models.CharField(max_length = 50)
-    password = models.CharField(max_length = 50)
+    home_address = models.CharField(max_length = 100, default="")
+    email_address = models.EmailField(default="", null=False)
+    username = models.CharField(max_length = 50, default="")
+    password = models.CharField(max_length = 50, default="")
     schedule_of_last_due = models.DateField() 
     schedule_of_next_payment = models.DateField() 
 
 class Rooms(models.Model):
-    room_number = models.IntegerField(unique=True, null=False)
-    room_description = models.TextField(null=False)
-    room_type = models.CharField(max_length = 20)
+    room_number = models.IntegerField(unique=True, default="", null=False)
+    room_description = models.TextField(default="", null=False)
+    room_type = models.CharField(max_length = 20, default="")
     room_amount = models.FloatField(null=False)
     room_status = models.CharField(max_length=20, null=False)
     no_of_beds_available = models.IntegerField(null=False)
@@ -30,21 +30,23 @@ class Beds(models.Model):
 
 class Reservation(models.Model):
     reservation_code = models.CharField(max_length= 50, unique = True)
-    reservation_date = models.DateField() 
-    reservation_time = models.TimeField()
+    reservation_date = models.DateField(auto_now_add = True) #date of reservation
+    tentative_checkin = models.DateField() #tentative date when to occupy the reservation
     lodgerID = models.IntegerField() #declare as foreign key to Lodger
 
 class Billing(models.Model):
-    ORnumber = models.IntegerField()
+    ORnumber = models.IntegerField(unique = True)
     lodgerID = models.IntegerField() #declare as foreign key to Lodger
     payment_date = models.DateField()
     payment_time = models.TimeField()
-    particulars = models.CharField(max_length = 100)
+    particulars = models.CharField(max_length = 200)
     amount_due = models.FloatField()
 
 class TransientCheckIn(models.Model):
     lodgerID = models.IntegerField() #declare as foreign key to Lodger
-    
+    checkinDate = models.DateField(auto_now_add = True)
+    checkinTime = models.TimeField()
+
 
 
 
